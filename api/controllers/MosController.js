1b685143c36cffb471151e5803986b23ae3f45dc
shras1993 <shras1993@gmail.com>
2016-07-30 07:38:51 +0000

    first commit

---
+++ api/controllers/MosController.js
@@ -0,0 +1,11 @@
+/**
+ * MosController
+ *
+ * @description :: Server-side logic for managing mos
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
---
+++ api/controllers/UserController.js
@@ -0,0 +1,52 @@
+/**
+ * UserController
+ *
+ * @description :: Server-side logic for managing Users
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+	 
+    index: function (req, res) {
+          res.view('UserIndex');
+     },
+
+  	list: function(req, res){
+  	    
+	    User.find()
+	    .then(function (users){
+	       // res.json(data);
+	       res.view('Userlist', {
+	           users: users
+	       });
+	    });
+	},
+	
+	page: function(req, res){
+	    res.view("Userpage");
+	},
+	
+	create: function (req, res) {
+	    var name = req.body.user;
+	    
+	    User.create ({
+	        name: name
+	    })
+	    .then(function ( user){
+	        res.redirect("/user/list")
+	    });
+	},
+	
+	destroy: function(req, res){
+	    var id = req.params['id'];
+	    
+	    User.destroy({
+	        id: id
+	    })
+	    .then(function (user){
+	        res.redirect("/user/list");
+	    });
+	}
+};
+
